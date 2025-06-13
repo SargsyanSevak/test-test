e755a41c3965773516cbab461c6bc22cfa6417f9
sargsyansevak <thesevaksargsyan@gmail.com>
2025-06-13 17:43:15 +0400

    added answer in the chatbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="w-full h-[3000px] flex flex-col gap-20 bg-gray-800 p-10">
+    <div className="w-full h-[3000px] flex flex-col gap-20 bg-[#050505] p-10">
       <div className="text-white pt-[200px]">
-          <p className="max-w-[700px] text-[20px] leading-[30px] font-bold">
+          <p className="max-w-[700px] text-[20px] leading-[30px] text-[#7f7f7f] font-semibold">
             Transform your business ideas into powerful digital solutions withs
             our trusted offshore development team. We deliver high quality
             software development at competitive rates while ensuring quality and
@@ -18,7 +18,7 @@ export default function Home() {
           className="w-fit"
         >
           <button
-            className="w-[196px] h-[56px] text-[20px] font-bold mt-4 bg-white"
+            className="w-[196px] leading-[30px] h-[56px] text-white font-semibold mt-4 bg-[#292929]"
           >
             Book a Call
           </button>
